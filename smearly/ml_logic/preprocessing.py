29f585a9a77e858b51f1233b2da4f5903d17e923
Fabien WAGON <fabgh-lewagon@azertyfab.net>
2025-03-12 13:04:05 +0100

    Added options to normalize or not & simplified resize_pad_image_tf code

--- smearly/ml_logic/preprocessing.py
+++ smearly/ml_logic/preprocessing.py
@@ -3,13 +3,14 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
-def resize_pad_image_tf(image: Any, target_size: tuple[int,int]=(224, 224)) -> tf.Tensor:
+def resize_pad_image_tf(image: Any, target_size: tuple[int,int]=(224, 224), normalize: bool=True) -> tf.Tensor:
     """
     Preprocesses an image by resizing, padding (with black), and normalizing it while maintaining the aspect ratio.
 
     Args:
-    - image: A Tensor of type string. 0-D. The PNG-encoded image.
+    - image: A Tensor of type string. 0-D. The PNG-encoded image. Hint: use image_file_to_tf() to generate this.
     - target_size (tuple): Target size (height, width) of the output image (default is 224x224).
+    - normalize (bool): pixel values will be divided by 255 if True, untouched otherwise
 
     The image function parameter can be produced with this code:
     ```
@@ -20,27 +21,19 @@ def resize_pad_image_tf(image: Any, target_size: tuple[int,int]=(224, 224)) -> t
     - image (tf.Tensor): The preprocessed image ready for model input.
     """
 
-    # Get original dimensions
-    orig_height, orig_width = image.shape[:2]
+    # Resize the image to the target size while maintaining aspect ratio and pad if necessary
+    image = tf.image.resize_with_pad(image, target_size[0], target_size[1], method='bilinear')
 
-    # Calculate the new size while maintaining the aspect ratio
-    if orig_width > orig_height:
-        new_width = target_size[0]
-        new_height = (orig_height * target_size[0]) // orig_width
+    if normalize is True:
+        # Normalize the image to [0, 1]
+        return tf.cast(image, tf.float32) / 255.0
     else:
-        new_height = target_size[1]
-        new_width = (orig_width * target_size[1]) // orig_height
+        # the resize above with any method other than 'nearest' will produce floats so...
+        # Ensure values are in the range [0, 255] (if they aren't already) and cnverted to uint8
+        image = tf.clip_by_value(image, 0.0, 255.0)
+        image = tf.cast(image, tf.uint8)
 
-    # Resize the image to the new dimensions (maintaining aspect ratio)
-    img_resized = tf.image.resize(image, (new_height, new_width), method='bilinear')
-
-    # Create a padded image (black padding) of target_size
-    img_padded = tf.image.resize_with_crop_or_pad(img_resized, target_size[0], target_size[1])
-
-    # Normalize the image to [0, 1]
-    img_normalized = tf.cast(img_padded, tf.float32) / 255.0
-
-    return img_normalized
+        return image
 
 
 def image_file_to_tf(image_path: str) -> tf.Tensor:
@@ -62,11 +55,19 @@ def image_file_to_tf(image_path: str) -> tf.Tensor:
     return tf.io.decode_png(image, channels=3)
 
 
-def create_image_dataset(directory: str, batch_size=32, target_size=(224, 224), shuffle=True, seed=42) -> tf.data.Dataset:
+def create_image_dataset(directory: str,
+                         batch_size: int=32,
+                         target_size: tuple[int,int] = (224, 224),
+                         shuffle: bool = True,
+                         seed: int = 42,
+                         pad: bool = True,
+                         normalize: bool=True) -> tf.data.Dataset:
     """
     Uses image_dataset_from_directory from tf to generate a tf.data.Dataset
     from image files in a directory. The directory must contain subdirectories
     named after the class and which contains the image files for each category.
+    Resizing cannot be avoided (but will probably be a no-op if source size
+    matches target size).
 
     Args:
     - directory: Path to the parent directory (containing subdirs)
@@ -80,7 +81,7 @@ def create_image_dataset(directory: str, batch_size=32, target_size=(224, 224), 
         # Normalize the pixel values to the range [0, 1]
         return image/255.0, label
 
-    return image_dataset_from_directory(
+    img_ds = image_dataset_from_directory(
         directory,
         image_size=target_size,
         batch_size=batch_size,
@@ -88,5 +89,9 @@ def create_image_dataset(directory: str, batch_size=32, target_size=(224, 224), 
         label_mode='categorical',
         shuffle=shuffle,
         seed=seed,
-        pad_to_aspect_ratio=True
-    ).map(normalize_image)
+        pad_to_aspect_ratio=pad
+    )
+    if normalize is True:
+        return img_ds.map(normalize_image)
+    else:
+        return img_ds
