44af87bec9228610b76cb37759dbce939d47828a
Eve Bazelaire <eve.avenati@icloud.com>
2025-03-13 17:22:33 +0100

    WIP prediction

--- smearly/ml_logic/prediction.py
+++ smearly/ml_logic/prediction.py
@@ -1,8 +1,21 @@
+import pandas as pd
 from tensorflow.keras.utils import to_categorical
 
+def encode_labels(y):
+    """
+    Encode labels to category encoding
+    :param y: labels
+    """
+    y_cat = to_categorical(y)
+    return y_cat
 
 
-y_cat_train = to_categorical(y_train)
-y_cat_train.shape, y_cat_train
-
+def prediction_to_csv(y_pred : pd.DataFrame, filename : str = "predictions.csv"):
+    """
+    Save predictions to CSV file
+    """
+    y_pred = y_pred.argmax(axis=1)
+    df = pd.DataFrame(y_pred, columns=["label"])
+    df.to_csv(filename, index=False)
+    print(f"✅ Predictions saved to {filename}")
 
