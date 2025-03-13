afebc81e485168e441226fe98de61769040f4e85
Fabien WAGON <fabgh-lewagon@azertyfab.net>
2025-03-13 16:49:36 +0100

    little improvements & cosmetic

--- smearly/tools/train_val_size.py
+++ smearly/tools/train_val_size.py
@@ -2,12 +2,19 @@
 import os
 
 def train_val_size(
-    val_distribution: list[float, int], # [rubbish, healthy, unhealthy]
-    train_distribution: list[float, int], # [rubbish, healthy, unhealthy]
+    val_distribution: list[float, int], # 3 numbers [rubbish, healthy, unhealthy]
+    train_distribution: list[float, int], # 3 numbers [rubbish, healthy, unhealthy]
     path: str = '../raw_data/all/unhealthy_bothcells_augmented',
     test_val_split: float = 0.3) -> dict:
     """
     Function to define the train and validation repartition with the desired number of images per class
+
+    Args:
+     - val_distribution - 3 numbers [rubbish, healthy, unhealthy]
+     - train_distribution - 3 numbers [rubbish, healthy, unhealthy]
+
+    Returns:
+     - a dictionary that can be used with generate_new_img_dir()
     """
 
     # Convertir les pourcentages en float (si ce sont des entiers)
@@ -29,9 +36,6 @@ def train_val_size(
     unhealthy_bothcells_augmented_and_unhealthy_bothcells_size = 2*len(os.listdir(path))
 
     total_size = unhealthy_bothcells_augmented_and_unhealthy_bothcells_size/train_distribution[2]
-    unhealthy_bothcells_augmented_and_unhealthy_bothcells_size , total_size
-    
-    test_val_split = test_val_split
 
     val_size = total_size*test_val_split
     train_size = total_size - val_size
@@ -42,27 +46,27 @@ def train_val_size(
     val_unhealthy_size = val_size*val_distribution[2]
     val_rubbish_size = val_size*val_distribution[0]
     val_healthy_size = val_size*val_distribution[1]
-    
+
     set_class_nb_files = {
-    'train': {
-        'bothcells': 0,
-        'healthy': round(train_healthy_size),
-        'rubbish': round(train_rubbish_size),
-        'unhealthy': 0,
-        'unhealthy_bothcells': round(train_unhealthy_size/2),
-        'unhealthy_bothcells_augmented': round(train_unhealthy_size/2)
-    },
-   
-   #must be different than train set
-   
-    'val': {
-        'bothcells': 0,
-        'healthy': round(val_healthy_size),
-        'rubbish': round(val_rubbish_size),
-        'unhealthy': 0,
-        'unhealthy_bothcells': round(val_unhealthy_size), 
-        'unhealthy_bothcells_augmented': 0
+        'train': {
+            'bothcells': 0,
+            'healthy': round(train_healthy_size),
+            'rubbish': round(train_rubbish_size),
+            'unhealthy': 0,
+            'unhealthy_bothcells': round(train_unhealthy_size/2),
+            'unhealthy_bothcells_augmented': round(train_unhealthy_size/2)
+        },
+
+    #must be different than train set
+
+        'val': {
+            'bothcells': 0,
+            'healthy': round(val_healthy_size),
+            'rubbish': round(val_rubbish_size),
+            'unhealthy': 0,
+            'unhealthy_bothcells': round(val_unhealthy_size),
+            'unhealthy_bothcells_augmented': 0
+        }
     }
-    }
-    
+
     return set_class_nb_files
