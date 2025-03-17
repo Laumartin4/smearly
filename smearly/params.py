c1dd11471d8f22b747c195ddf55412b9831b0965
Fabien WAGON <fabgh-lewagon@azertyfab.net>
2025-03-17 15:57:23 +0100

    params.py fixup

--- smearly/params.py
+++ smearly/params.py
@@ -2,4 +2,4 @@ import os
 
 # defaults to local model saving & loading
 MODEL_SAVING_MODE = os.environ.get("MODEL_SAVING_MODE", "local")
-MODEL_LOADING_MODE = int(os.environ.get("MODEL_SAVING_MODE", "local"))
+MODEL_LOADING_MODE = os.environ.get("MODEL_LOADING_MODE", "local")
